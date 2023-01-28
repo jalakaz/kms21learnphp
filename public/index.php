e3895068a9f2e9a72822c7e2a59759e7e438648e
Andra Jalakas <KMS-21-904@tptlive.ee>
2023-01-28 11:40:34 +0200

    edit

--- public/index.php
+++ public/index.php
@@ -4,11 +4,7 @@ use App\Controllers\ArticleController;
 use App\Router;
 
 session_start();
-
-spl_autoload_register(function($class){
-    $class = substr($class, strlen('App\\'));
-    require_once "src/$class.php";
-});
+require __DIR__ .  '/../vendor/autoload.php';
 require 'helpers.php';
 require 'routes.php';
 
